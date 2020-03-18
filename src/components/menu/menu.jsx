a82f798b06d95307befafec943e91417217b42cd
ssaveski <saveskistef@gmai.l.com>
2020-03-18 08:44:03 +0100

    initial commit

---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from 'react';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Button from "@material-ui/core/Button";
+
+
+const Menu = ({data}) => {
+    const [menu, setMenu] = useState([]);
+    const [expanded, setExpanded] = useState([]);
+    const [areAllExpanded, setAreAllExpanded] = useState(false);
+    const [allExpandableNodes, setAllExpandableNodes] = useState([]);
+
+    let getAllNodes = [];
+
+    useEffect(() => {
+        setMenu(displayTree(data));
+        takeAllExpandableNodes(data);
+    }, []);
+
+    useEffect(() => {
+        if (allExpandableNodes.length > 1)
+            setAreAllExpanded(expanded.toString() === allExpandableNodes.toString());
+    }, [expanded]);
+
+    function displayTree(jsonData) {
+        let newMenu = null;
+        newMenu = jsonData.map((branch, index) => {
+            return (
+                <TreeItem nodeId={branch.id} label={branch.label} key={index}>
+                    {branch.children ? newMenu = (displayTree(branch.children)) : null}
+                </TreeItem>
+            );
+        });
+        return newMenu;
+    }
+
+    const handleChange = (event, nodes) => {
+        nodes.sort();
+        setExpanded(nodes);
+    };
+
+    function takeAllExpandableNodes(nodes) {
+        nodes.map((branch) => {
+            if (branch.id && branch.children.length > 0)
+                getAllNodes.push(branch.id);
+            if (branch.children)
+                takeAllExpandableNodes(branch.children)
+        });
+        setAllExpandableNodes(getAllNodes.sort());
+    }
+
+    const toggleExpandCollapseAll = (areAllExpanded) => {
+        !areAllExpanded ? setExpanded(allExpandableNodes) : setExpanded([])
+    };
+
+    return (
+        <React.Fragment>
+            <TreeView
+                defaultCollapseIcon={<ExpandMoreIcon/>}
+                defaultExpandIcon={<ChevronRightIcon/>}
+                onNodeToggle={handleChange}
+                expanded={expanded}
+            >
+                {menu}
+            </TreeView>
+            <Button variant="contained" color="secondary" className="btn"
+                    onClick={() => toggleExpandCollapseAll(areAllExpanded)}>
+                {!areAllExpanded ? "Expand All" : "Collapse All"}
+            </Button>
+        </React.Fragment>
+    );
+
+};
+
+export default Menu;
